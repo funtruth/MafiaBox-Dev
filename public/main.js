ac80abff2b916e985bd94cbaefcc28b1e804a700
ouoru <mike5_17@hotmail.com>
2018-10-15 16:21:51 -0400

    dist

--- public/main.js
+++ public/main.js
@@ -1,11 +1,15 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
   mainWindow.on('closed', () => mainWindow = null);
 }
