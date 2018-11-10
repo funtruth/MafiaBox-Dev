766779e6aa4646319e102f7f3012085c7beac287
ouoru <mike5_17@hotmail.com>
2018-11-09 21:45:15 -0500

    codemirror sample

--- src/modules/page/PageView.js
+++ src/modules/page/PageView.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './page.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
 import { connect } from 'react-redux'
 
 import { updatePage } from './PageReducer'
@@ -9,7 +11,16 @@ import PageAbstract from './components/PageAbstract'
 import InputField from './components/InputField'
 import AddNewField from './components/AddNewField'
 
+import {Controlled as CodeMirror} from 'react-codemirror2'
+require('codemirror/mode/javascript/javascript')
+
 class PageView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+    }
     render() {
         const { pageKey, pageRepo } = this.props
         const pageInfo = pageRepo[pageKey]
@@ -25,6 +36,15 @@ class PageView extends React.Component {
                     <InputField pageInfo={pageInfo} updatePage={this.props.updatePage} field="roleHelo"/>
                     <AddNewField/>
                 </div>
+                <CodeMirror
+                    value={this.state.value}
+                    options={{
+                        mode: 'javascript',
+                        theme: 'material',
+                        lineNumbers: true
+                    }}
+                    onBeforeChange={(editor, data, value) => this.setState({value})}
+                />
             </div>
         )
     }
